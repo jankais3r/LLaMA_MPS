a333821fb7cf2ce2e803309b773dc82410279033
Jan Kaiser <2159464+jankais3r@users.noreply.github.com>
2023-03-08 09:37:11 +0100

    Initial release

---
+++ README.md
@@ -0,0 +1,60 @@
+# LLaMA_MPS
+Run LLaMA inference on Apple Silicon GPUs.
+
+![Demo](demo.gif)
+
+### Setup
+
+**1. Clone this repo**
+
+`git clone https://github.com/jankais3r/LLaMA_MPS`
+
+**2. [Download the model weights](https://github.com/facebookresearch/llama/pull/73/files#diff-b335630551682c19a781afebcf4d07bf978fb1f8ac04c6bf87428ed5106870f5R4) and put them into a folder called** `models` (e.g., `LLaMA_MPS/models/7B`)
+
+**3. Install Python dependencies**
+
+```bash
+pip3 install -r requirements.txt
+pip3 install -e .
+```
+
+**4. _(Optional)_ Reshard the model (13B/30B/65B)**
+
+Since we are running the inference on a single GPU, we need to merge the larger models' weights into a single file.
+
+```bash
+mv models/13B models/13B_orig
+mkdir models/13B
+python3 reshard.py 1 models/13B_orig models/13B
+```
+
+**5. Run the inference**
+
+`PYTORCH_ENABLE_MPS_FALLBACK=1 torchrun chat.py --ckpt_dir models/13B  --tokenizer_path models/tokenizer.model --max_batch_size=8`
+
+### Memory requirements
+
+| Model  | Peak memory during load | Memory during inference |
+| ------------- | ------------- | ------------- |
+| 7B  | 25 GB  | 16 GB  |
+| 13B  | 66 GB  | 32 GB  |
+| 30B  | ?? GB  | ?? GB  |
+| 65B  | ?? GB  | ?? GB  |
+
+### Parameters to experiment with
+**- max_batch_size**
+
+If you are running low on memory, you can reduce `max_batch_size` to 1.
+
+**- max_gen_len**
+
+To increase/reduce the length of the generated text, edit the `max_gen_len` value in [chat.py](https://github.com/jankais3r/LLaMA_MPS/blob/main/chat.py#L83).
+
+
+### Credits
+
+- facebookresearch ([original code](https://github.com/facebookresearch/llama))
+- markasoftware ([cpu optimizations](https://github.com/markasoftware/llama-cpu))
+- remixer-dec ([mps optimizations](https://github.com/remixer-dec/llama-mps))
+- venuatu ([continuous token printing](https://github.com/venuatu/llama/commit/25c84973f71877677547453dab77eeaea9a86376))
+- benob ([reshard script](https://gist.github.com/benob/4850a0210b01672175942203aa36d300))
---
+++ chat.py
@@ -0,0 +1,92 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# This software may be used and distributed according to the terms of the GNU General Public License version 3.
+
+import os
+import sys
+import torch
+import fire
+import time
+import json
+import warnings
+from typing import Tuple
+from pathlib import Path
+warnings.filterwarnings("ignore")
+from llama import ModelArgs, Transformer, Tokenizer, LLaMA
+from fairscale.nn.model_parallel.initialize import initialize_model_parallel
+
+
+def setup_model_parallel() -> Tuple[int, int]:
+    local_rank = int(os.environ.get("LOCAL_RANK", -1))
+    world_size = int(os.environ.get("WORLD_SIZE", -1))
+
+    torch.distributed.init_process_group("gloo")
+    initialize_model_parallel(world_size)
+    torch.manual_seed(1)
+    return local_rank, world_size
+
+
+def load(
+    ckpt_dir: str, 
+    tokenizer_path: str, 
+    local_rank: int, 
+    world_size: int, 
+    max_seq_len: int, 
+    max_batch_size: int, 
+) -> LLaMA:
+    start_time = time.time()
+    checkpoints = sorted(Path(ckpt_dir).glob("*.pth"))
+    assert world_size == len(
+        checkpoints
+    ), f"Loading a checkpoint for MP = {len(checkpoints)} but world size is {world_size}"
+    ckpt_path = checkpoints[local_rank]
+    print("Loading")
+    checkpoint = torch.load(ckpt_path, map_location = "cpu")
+    with open(Path(ckpt_dir) / "params.json", "r") as f:
+        params = json.loads(f.read())
+
+    model_args: ModelArgs = ModelArgs(
+        max_seq_len = max_seq_len, max_batch_size = max_batch_size, **params
+    )
+    tokenizer = Tokenizer(model_path = tokenizer_path)
+    model_args.vocab_size = tokenizer.n_words
+    torch.set_default_tensor_type(torch.HalfTensor)
+    model = Transformer(model_args)
+    torch.set_default_tensor_type(torch.FloatTensor)
+    model.load_state_dict(checkpoint, strict = False)
+    model = model.to("mps")
+    generator = LLaMA(model, tokenizer)
+    print(f"Loaded in {time.time() - start_time:.2f} seconds")
+    return generator
+
+
+def main(
+    ckpt_dir: str, 
+    tokenizer_path: str, 
+    temperature: float = 0.8, 
+    top_p: float = 0.95, 
+    max_seq_len: int = 512, 
+    max_batch_size: int = 1):
+    local_rank, world_size = setup_model_parallel()
+    if local_rank > 0:
+        sys.stdout = open(os.devnull, "w")
+
+    generator = load(
+        ckpt_dir, tokenizer_path, local_rank, world_size, max_seq_len, max_batch_size
+    )
+
+    try:
+        while True:
+            queryInputs = [input("Enter your LLaMA prompt: ")]
+            print("Thinking...")
+            queryTime = time.time()
+            results = generator.generate(
+                queryInputs, max_gen_len = 512, temperature = temperature, top_p = top_p
+            )
+            print(f"\nInferred in {time.time() - queryTime:.2f} seconds")
+            print("==================================\n")
+    except KeyboardInterrupt:
+        pass
+
+
+if __name__ == "__main__":
+    fire.Fire(main)
